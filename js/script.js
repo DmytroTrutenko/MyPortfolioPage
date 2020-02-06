ec78d4a15cc55f98de22d23c766a2c4a52fd54f2
DmytroTrutenko <55551711+DmytroTrutenko@users.noreply.github.com>
2020-02-07 01:15:19 +0200

    up portfolio!

---
+++ js/script.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $("#main-nav").on("click","a", function (event) {
+        event.preventDefault();
+        var id  = $(this).attr('href'),
+            top = $(id).offset().top;
+        $('body,html').animate({scrollTop: top}, 1000);
+    });
+});
